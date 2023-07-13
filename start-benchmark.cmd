ffd67d0baf7628d86de9d66336a5b3e6a4f6f9bb
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-07-13 09:17:02 +0530

    release mode

--- start-benchmark.cmd
+++ start-benchmark.cmd
@@ -1,4 +1,4 @@
-start cmd.exe /C "cd Broker & title Broker & dotnet run"
-start cmd.exe /C "cd Subscriber & title Subscriber & dotnet run"
-start cmd.exe /C "cd Publisher & title Publisher & dotnet run"
+start cmd.exe /C "cd Broker & title Broker & dotnet run -c Release"
+start cmd.exe /C "cd Subscriber & title Subscriber & dotnet run -c Release"
+start cmd.exe /C "cd Publisher & title Publisher & dotnet run -c Release"
 start http://localhost:5000
